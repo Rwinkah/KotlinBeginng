39a674a66049d72146f53a33c61d8459e797459e
Rwinkah <84604851+Rwinkah@users.noreply.github.com>
2021-07-03 04:56:13 +0100

    Add files via upload
    First Try Debug please

---
+++ Practice.kts
@@ -0,0 +1,12 @@
+
+ fun main(args: Array<String>) {
+     println("How many hours was yout car parked for?")
+
+     var hours = readline()!!.toInt()
+     var total: Double = 0.0
+     if (hours < 24) {
+         total = 5 + ((hours - 5) * 0.5)
+
+
+     } else total = 15 + (hours - 24) * 0.5
+ }
