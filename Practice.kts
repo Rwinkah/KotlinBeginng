ff5e453e94fd0709042b7dc58b87ec63ba4e384b
GZMaster <danigamester@gmail.com>
2021-07-03 13:13:04 +0100

    the readLine command had a typo hence the bug

--- Practice.kts
+++ Practice.kts
@@ -2,7 +2,7 @@
  fun main(args: Array<String>) {
      println("How many hours was yout car parked for?")
 
-     var hours = readline()!!.toInt()
+     var hours = readLine()!!.toInt()
      var total: Double = 0.0
      if (hours < 24) {
          total = 5 + ((hours - 5) * 0.5)
